7aabbd50d002d75343f7f6794eb72969649bee2c
sofiagunkalo <sofiikahunkalo@gmail.com>
2026-04-27 22:27:30 +0300

    Added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
+import { Footer } from "./components/Footer";
 
 const App = () => {
   return (
@@ -10,8 +11,8 @@ const App = () => {
           <h1>Welcome to Сантех Студія!</h1>
         </Container>
       </main>      
+      <Footer />
     </>
   );
 };
-
-export default App
+export default App;
