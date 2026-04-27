3ba74d724e945f51d5bb78a68ee8541a07395ebe
sofiagunkalo <sofiikahunkalo@gmail.com>
2026-04-27 22:07:31 +0300

    Added Header.jsx

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect> 
+        <Container>
+          <Navbar.Brand href="/">Сантех Студія</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart"><FaShoppingCart /> Кошик</Nav.Link>
+              <Nav.Link href="/login"><FaUser /> Увійти</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+export default Header
