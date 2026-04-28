15ee7a71f7c4e254dd2242fe98a341abd8898abf
sofiagunkalo <sofiikahunkalo@gmail.com>
2026-04-28 19:52:32 +0300

    Changed favicons for different browsers/devices

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.png'
 
 const Header = () => {
   return (
